7d19b038f0debc8907b5d62b9b521e6fbb50b97e
ShurGith <75266693+ShurGith@users.noreply.github.com>
2024-09-04 12:26:22 +0200

    Inicio

--- Contenedor/Taller/settings.py
+++ Contenedor/Taller/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)%kdo#y(rwuuyh9g=fm$lom7x&)u)v%4r(dtj+i!vgs6h^=g+i'
+SECRET_KEY = 'django-insecure-r-68(c(il%tk%y2f744g^434bc%-ll4j$@#og(ismv)v3xq+*e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
